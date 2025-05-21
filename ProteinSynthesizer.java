61e915b07228ffb1313973c5583b1a93f0a1377c
pratikae <75759431+pratikae@users.noreply.github.com>
2025-05-21 01:08:48 -0700

    Remove exceptions, Clean code

--- ProteinSynthesizer.java
+++ ProteinSynthesizer.java
@@ -1,86 +1,156 @@
 import java.util.*;
-import java.io.*;
 
 public class ProteinSynthesizer extends MRNAConverter {
-    static String getProtein(String codon) {
-
-        if (codon.equals("TTT") || codon.equals("TTC")) {
-            return "Phe";
-        } else if (codon.equals("TTA") || codon.equals("TTG") || codon.equals("CTT") || codon.equals("CTC") || codon.equals("CTA") || codon.equals("CTG")) {
-            return "Lue";
-        } else if (codon.equals("ATT") || codon.equals("ATC") || codon.equals("ATA")) {
-            return "Ile";
-        } else if (codon.equals("ATG")) {
-            return "Met";
-        } else if (codon.substring(0, 2).equals("GT")) {
-            return "Val";
-        } else if (codon.substring(0, 2).equals("TC")) {
-            return "Ser";
-        } else if (codon.substring(0, 2).equals("CC")) {
-            return "Pro";
-        } else if (codon.substring(0, 2).equals("AC")) {
-            return "Thr";
-        } else if (codon.substring(0, 2).equals("GC")) {
-            return "Ala";
-        } else if (codon.substring(0, 2).equals("CG")) {
-            return "Arg";
-        } else if (codon.substring(0, 2).equals("GG")) {
-            return "Gly";
-        } else if (codon.equals("TAT") || codon.equals("TAC")) {
-            return "Tyr";
-        } else if (codon.equals("CAT") || codon.equals("CAC")) {
-            return "His";
-        } else if (codon.equals("CCA") || codon.equals("CAG")) {
-            return "Gln";
-        } else if (codon.equals("AAT") || codon.equals("AAC")) {
-            return "Asn";
-        } else if (codon.equals("AAA") || codon.equals("AAG")) {
-            return "Lys";
-        } else if (codon.equals("GAT") || codon.equals("GAC")) {
-            return "Asp";
-        } else if (codon.equals("GAA") || codon.equals("GAG")) {
-            return "Glu";
-        } else if (codon.equals("TGT") || codon.equals("TGC")) {
-            return "Cys";
-        } else if (codon.equals("TGG")) {
-            return "Trp";
-        } else if (codon.equals("AGT") || codon.equals("AGC")) {
-            return "Ser";
-        } else if (codon.equals("AGA") || codon.equals("AGG")) {
-            return "Arg";
-        } else if (codon.equals("TAA") || codon.equals("TAG") || codon.equals("TGA")) {
-            return "STOP";
-        } else {
-            return "";
+    static String codonWheel(String codon) {
+        switch (codon.charAt(0)) {
+            case 'U':
+                switch (codon.charAt(1)) {
+                    case 'U':
+                        if (codon.charAt(2) == 'U' || codon.charAt(2) == 'C') {
+                            return  "phe";
+                        } else {
+                            return  "leu";
+                        }
+                    case 'C':
+                        return  "ser";
+                    case 'A':
+                        if (codon.charAt(2) == 'U' || codon.charAt(2) == 'C') {
+                            return  "tyr";
+                        } else {
+                            return  "STOP";
+                        }
+                    case 'G':
+                        if (codon.charAt(2) == 'U' || codon.charAt(2) == 'C') {
+                            return  "cys";
+                        } else if (codon.charAt(2) == 'G') {
+                            return  "trp";
+                        } else {
+                            return "STOP";
+                        }
+                    default:
+                        return "invalid codon";
+                }
+            case 'C':
+                switch (codon.charAt(1)) {
+                    case 'U':
+                        return "leu";
+                    case 'C':
+                        return "pro";
+                    case 'A':
+                        if (codon.charAt(2) == 'U' || codon.charAt(2) == 'C') {
+                            return "his";
+                        } else {
+                            return "gln";
+                        }
+                    case 'G':
+                        return "arg";
+                    default:
+                        return "invalid codon";
+                }
+            case 'A':
+                switch (codon.charAt(1)) {
+                    case 'U':
+                        if (codon.charAt(2) == 'G') {
+                            return "met";
+                        } else {
+                            return "ile";
+                        }
+                    case 'C':
+                        return "thr";
+                    case 'A':
+                        if (codon.charAt(2) == 'G' || codon.charAt(2) == 'C') {
+                            return "asn";
+                        } else {
+                            return "lys";
+                        }
+                    case 'G':
+                        if (codon.charAt(2) == 'G' || codon.charAt(2) == 'C') {
+                            return "ser";
+                        } else {
+                            return "arg";
+                        }
+                }
+            case 'G':
+                switch (codon.charAt(1)) {
+                    case 'U':
+                        return "val";
+                    case 'C':
+                        return "ala";
+                    case 'A':
+                        if (codon.charAt(2) == 'G' || codon.charAt(2) == 'C') {
+                            return "asp";
+                        } else {
+                            return "glu";
+                        }
+                    case 'G':
+                        return "gly";
+                }
+            default:
+                return "invalid codon";
         }
-
     }
 
     static void printProteins(String mRNA) {
-        if (mRNA.length() % 3 != 0) {
-            return;
+        int startIndex = -1;
+        // establish reading frame!
+        for (int i = 0; i < mRNA.length() - 3; i++) {
+            if (mRNA.charAt(i) == 'A' && mRNA.charAt(i + 1) == 'U' && mRNA.charAt(i + 2) == 'G') {
+                startIndex = i;
+                break;
+            }
         }
 
-        int i = 0;
-        while (i < mRNA.length()) {
-            String codon = mRNA.substring(i, i + 3);
-            String protein = getProtein(codon);
-            if (!protein.equals("STOP")) {
-                System.out.println(protein);
-            } else {
-                return;
+        // translation!
+        if (startIndex == -1) {
+            System.out.println("start codon not found, unable to translate");
+        } else {
+            for (int i = startIndex; i < mRNA.length() - 3; i += 3) {
+                String codon = mRNA.substring(i, i + 3);
+                String aminoAcid = codonWheel(codon);
+                if (!aminoAcid.equals("STOP")) {
+                    System.out.print(aminoAcid + " ");
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter 1 for coding strand and 2 for template strand (0 to exit) >>> ");
+            int strandType = console.nextInt();
+
+            while (strandType != 1 && strandType != 2 && strandType != 0) {
+                System.out.println("invalid input, try again");
+                System.out.print("Enter 1 for coding strand and 2 for template strand (0 to exit) >>> ");
+                strandType = console.nextInt();
             }
 
-            i += 3;
+            if (strandType == 1) {
+                System.out.print("Enter the DNA coding strand 5' -> 3'(enter 0 to exit) >>> ");
+            } else if (strandType == 2) {
+                System.out.print("Enter the DNA template strand 5' -> 3' (enter 0 to exit) >>> ");
+            } else {
+                break;
+            }
+
+            String DNA = console.next().toUpperCase();
+            if (DNA.equals("0")) {
+                break;
+            }
+
+            if (validDNA(DNA)) {
+                String mRNA = getMRNA(DNA, strandType);
+                System.out.println("Transcribed mRNA sequence >>> " + mRNA);
+                System.out.print("Translated protein >>> ");
+                printProteins(mRNA);
+                System.out.println();
+            } else {
+                System.out.println("Invalid DNA string");
+            }
+            System.out.println();
         }
     }
-
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner console = new Scanner(new File("input.txt"));
-        while (console.hasNext()) {
-            printProteins(getMRNA(console.next()));
-        }
-    }
-
-
 }
